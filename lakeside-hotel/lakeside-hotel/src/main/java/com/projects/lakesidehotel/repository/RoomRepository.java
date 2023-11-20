7ccc21ed1d4840e93654214685fe9c069195b315
akhil <akhilgolla003@gmail.com>
2023-11-19 19:29:50 -0600

    implemented a add functionality and api to fetch all room types

--- lakeside-hotel/lakeside-hotel/src/main/java/com/projects/lakesidehotel/repository/RoomRepository.java
+++ lakeside-hotel/lakeside-hotel/src/main/java/com/projects/lakesidehotel/repository/RoomRepository.java
@@ -1,4 +1,15 @@
 package com.projects.lakesidehotel.repository;
 
-public interface RoomRepository {
+import com.projects.lakesidehotel.model.Room;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface RoomRepository extends JpaRepository<Room, Long> {
+
+    @Query("SELECT DISTINCT r.roomType FROM Room r")
+    List<String> findDistinctRoomTypes();
 }
--- lakeside-hotel/lakeside-hotel/src/main/java/com/projects/lakesidehotel/service/RoomServiceImpl.java
+++ lakeside-hotel/lakeside-hotel/src/main/java/com/projects/lakesidehotel/service/RoomServiceImpl.java
@@ -1,4 +1,42 @@
 package com.projects.lakesidehotel.service;
 
-public class RoomServiceImpl {
+import com.projects.lakesidehotel.model.Room;
+import com.projects.lakesidehotel.repository.RoomRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.sql.rowset.serial.SerialBlob;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.sql.Blob;
+import java.sql.SQLException;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class RoomServiceImpl implements RoomService{
+
+    private final RoomRepository roomRepository;
+
+    @Override
+    public Room addNewRoom(MultipartFile file, String roomType, BigDecimal roomPrice) throws IOException, SQLException {
+
+        Room room = new Room();
+        room.setRoomType(roomType);
+        room.setRoomPrice(roomPrice);
+        if(!file.isEmpty())
+        {
+            byte[] photoBytes = file.getBytes();
+            Blob photoBlob = new SerialBlob(photoBytes);
+            room.setPhoto(photoBlob);
+        }
+
+        return roomRepository.save(room);
+    }
+
+    @Override
+    public List<String> getAllRoomTypes() {
+        return roomRepository.findDistinctRoomTypes();
+    }
 }
