a3c4ea76a2cc762817447be8533a7076c019b460
akhil <gollaakhil1115@gmail.com>
2023-11-25 21:25:56 -0600

    added a page to browse all rooms

--- lakeview-client/src/components/layout/NavBar.jsx
+++ lakeview-client/src/components/layout/NavBar.jsx
@@ -30,13 +30,23 @@ const NavBar = () => {
         id="navbarScroll">
             <ul className="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll">
                 <li className="nav-item">
+					<Link className="nav-link" 
+                    aria-current="page" 
+                    to={"/browse-all-rooms"}>
+						Browse all rooms
+					</Link>
+				</li>
+
+
+
+                {/* <li className="nav-item">
                     <NavLink className="nav-link "
                     aria-current="page"
                     to={"/browse-all-rooms"}>
                         Browse all rooms
                     </NavLink>
                     
-                </li>
+                </li> */}
 
                 <li className="nav-item">
                     <NavLink className="nav-link "
