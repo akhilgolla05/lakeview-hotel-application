950dcd1e98db4e99d3cba4c197625d5d66100afe
akhil <gollaakhil1115@gmail.com>
2023-11-26 22:01:28 -0600

    designed home page with proper content and images

--- lakeview-client/src/components/home/Home.jsx
+++ lakeview-client/src/components/home/Home.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import HeaderMain from '../layout/HeaderMain'
+import HotelService from '../common/HotelService'
+import Parallax from '../common/Parallax'
 
 const Home = () => {
   return (
-    <div>
-      <h2>Welcome to the Home Page</h2>
-    </div>
+    <section>
+      <HeaderMain/>
+      <section className='container'>
+        <Parallax/>
+        <HotelService/>
+        <Parallax/>
+
+      </section>
+
+    </section>
   )
 }
 
