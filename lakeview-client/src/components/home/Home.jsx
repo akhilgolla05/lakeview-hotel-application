280817a931bf42045c58fc4b295b8991e23de8f4
akhil <gollaakhil1115@gmail.com>
2023-11-27 21:37:46 -0600

    implemented Carousel in the home page section

--- lakeview-client/src/components/home/Home.jsx
+++ lakeview-client/src/components/home/Home.jsx
@@ -2,15 +2,19 @@ import React from 'react'
 import HeaderMain from '../layout/HeaderMain'
 import HotelService from '../common/HotelService'
 import Parallax from '../common/Parallax'
+import RoomCarousel from '../common/RoomCarousel'
 
 const Home = () => {
   return (
     <section>
       <HeaderMain/>
       <section className='container'>
+        <RoomCarousel/>
         <Parallax/>
+        <RoomCarousel/>
         <HotelService/>
         <Parallax/>
+        <RoomCarousel/>
 
       </section>
 
