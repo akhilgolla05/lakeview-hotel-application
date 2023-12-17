7c097d1cf5e7b657afeaf493d7ce983a6b47c9c1
akhil <gollaakhil1115@gmail.com>
2023-12-17 01:43:01 -0600

    implemented functionality to search available rooms on specific dates

--- lakeside-hotel/lakeside-hotel/src/main/java/com/projects/lakesidehotel/controller/RoomController.java
+++ lakeside-hotel/lakeside-hotel/src/main/java/com/projects/lakesidehotel/controller/RoomController.java
@@ -10,6 +10,7 @@ import com.projects.lakesidehotel.service.BookedRoomService;
 import com.projects.lakesidehotel.service.RoomService;
 import lombok.RequiredArgsConstructor;
 import org.apache.tomcat.util.codec.binary.Base64;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,6 +21,7 @@ import java.io.IOException;
 import java.math.BigDecimal;
 import java.sql.Blob;
 import java.sql.SQLException;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -148,5 +150,29 @@ public class RoomController {
         }).orElseThrow(()-> new ResourceNotFoundException("Room not Found"));
     }
 
+
+    //get avilable rooms
+    @GetMapping("/available-rooms")
+    public ResponseEntity<List<RoomResponse>> getAvailableRooms(
+            @RequestParam("checkInDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)LocalDate checkInDate,
+            @RequestParam("checkOutDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)LocalDate checkOutDate,
+            @RequestParam("roomType") String roomType) throws SQLException {
+        List<Room> availableRooms = roomService.getAvailableRooms(checkInDate, checkOutDate, roomType);
+        List<RoomResponse> roomResponses = new ArrayList<>();
+        for (Room room : availableRooms){
+            byte[] photoBytes = roomService.getRoomPhotoByRoomId(room.getId());
+            if (photoBytes != null && photoBytes.length > 0){
+                String photoBase64 = Base64.encodeBase64String(photoBytes);
+                RoomResponse roomResponse = getRoomResponse(room);
+                roomResponse.setPhoto(photoBase64);
+                roomResponses.add(roomResponse);
+            }
+        }
+        if(roomResponses.isEmpty()){
+            return ResponseEntity.noContent().build();
+        }else{
+            return ResponseEntity.ok(roomResponses);
+        }
+    }
 
 }
--- lakeside-hotel/lakeside-hotel/src/main/java/com/projects/lakesidehotel/service/RoomServiceImpl.java
+++ lakeside-hotel/lakeside-hotel/src/main/java/com/projects/lakesidehotel/service/RoomServiceImpl.java
@@ -13,6 +13,7 @@ import java.io.IOException;
 import java.math.BigDecimal;
 import java.sql.Blob;
 import java.sql.SQLException;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -96,5 +97,10 @@ public class RoomServiceImpl implements RoomService{
     public Optional<Room> getRoomById(Long roomId) {
 
         return Optional.of(roomRepository.findById(roomId).get());
+    }
+
+    @Override
+    public List<Room> getAvailableRooms(LocalDate checkInDate, LocalDate checkOutDate, String roomType) {
+        return roomRepository.findAvailableRoomsByDateAndType(checkInDate, checkOutDate, roomType);
     }
 }
